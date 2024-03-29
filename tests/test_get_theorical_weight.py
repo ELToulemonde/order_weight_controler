e70cc73e55728c558bae8dce5b39065a6bb296a8
TOUL <toul>
2024-03-29 15:01:37 +0100

    truly fixed test

--- tests/test_get_theorical_weight.py
+++ tests/test_get_theorical_weight.py
@@ -6,13 +6,13 @@ from weight_controler_api.get_theorical_weight import get_theorical_weight
 
 class Test(TestCase):
     @patch('weight_controler_api.get_theorical_weight.load_order')
-    def test_get_theorical_weight(self,mock_load_order):
+    def test_get_theorical_weight(self, mock_load_order):
         # given
         given_order_id = "fc71b64b-a3a3-433d-9faf-cbdb45c8734b"
         expected_weight = 100
         mock_load_order.return_value = {
             "order_id": given_order_id,
-            "content" : [{
+            "content": [{
                 "product_id": 15, "product_name": "Big Tasty", "quantity": 1
             }]
         }
