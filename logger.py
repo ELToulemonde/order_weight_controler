41e35352cbd300b1bab9cf8bb37ce8ba9e0d3fca
TOUL <toul>
2024-03-29 09:22:31 +0100

    REVIEW

--- logger.py
+++ logger.py
@@ -1,7 +1,5 @@
 import logging
 
-# Configuration du logger
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
-# Création d'un objet logger
 logger = logging.getLogger(__name__)
--- weight_controler_api/save_control.py
+++ weight_controler_api/save_control.py
@@ -1,10 +1,13 @@
 import sqlite3
+from os.path import join
 
 import pandas as pd
+
+from data import DATA_PATH
 
 
 def save_control(order_id, test_weight, weight_theorical, weight_real, employee_id):
     df = pd.DataFrame({"order_id": [order_id], "theoretical_weight": [weight_theorical], "weight_real": [weight_real],
-                       "test_weight": [test_weight],"employee_id": [employee_id]})
-    conn = sqlite3.connect('weight_controller.db')
+                       "test_weight": [test_weight], "employee_id": [employee_id]})
+    conn = sqlite3.connect(join(DATA_PATH, 'weight_controller.db'))
     df.to_sql("hist_control", conn, if_exists="append")
